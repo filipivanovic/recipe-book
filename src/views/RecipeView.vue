ad95961ea583100a7df2281728f3b034b4e6f614
Filip Ivanovic <filip_ivanovic@hotmail.com>
2024-09-16 21:58:07 +0200

    Create RecipeView.vue

---
+++ src/views/RecipeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+</script>
+
+<template>
+  <div>Recipe View {{ route.params.id }}</div>
+</template>
+
+<style scoped>
+
+</style>
