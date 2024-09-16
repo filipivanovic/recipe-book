e370bc8eec522e5a5d94014b1bdde2b3aa4ec109
Filip Ivanovic <filip_ivanovic@hotmail.com>
2024-09-16 22:53:05 +0200

    Add RouterLink to HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+import { RouterLink } from 'vue-router'
+
+</script>
 
 <template>
-  <main>home</main>
+  <main>
+    <h1>Home</h1>
+    <nav>
+      <ul>
+        <li>
+          <RouterLink :to="{name: 'recipe', params:{id: 1}}">Recipe 1</RouterLink>
+        </li>
+        <li>
+          <RouterLink :to="{name: 'recipe', params:{id: 2}}">Recipe 2</RouterLink>
+        </li>
+      </ul>
+    </nav>
+  </main>
 </template>
