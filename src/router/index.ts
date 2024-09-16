16476e4049d7736ed2f6df09e92d8b1c88fb90f2
Filip Ivanovic <filip_ivanovic@hotmail.com>
2024-09-16 21:51:13 +0200

    Create AddRecipeView.vue and FavoritesView.vue components

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import FavoritesView from '@/views/FavoritesView.vue'
+import AddRecipeView from '@/views/AddRecipeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/favorites',
+      name: 'favorites',
+      component: FavoritesView
+    },
+    {
+      path: '/add-recipe',
+      name: 'add-recipe',
+      component: AddRecipeView
     }
   ]
 })
---
+++ src/views/FavoritesView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>Favs</div>
+</template>
+
+<style scoped>
+
+</style>
