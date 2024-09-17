0741c4e1f75edc62377f7a72634f289f47c00e59
Filip Ivanovic <filip_ivanovic@hotmail.com>
2024-09-17 15:45:40 +0200

    Add watcher for initializing RecipeView

--- src/views/RecipeView.vue
+++ src/views/RecipeView.vue
@@ -1,13 +1,28 @@
 <script setup lang="ts">
 
 import { useRoute } from 'vue-router'
+import { onMounted, watch } from 'vue'
 
 const route = useRoute()
+
+onMounted(() => console.log('Fetching data inside onMounted'))
+
+watch(() => route.params.id, () => console.log('Fetching inside watch'), { immediate: true })
 
 </script>
 
 <template>
   <div>Recipe View {{ route.params.id }}</div>
+  <nav>
+    <ul>
+      <li>
+        <RouterLink :to="{name: 'recipe', params: {id: 1}}">Recipe 1</RouterLink>
+      </li>
+      <li>
+        <RouterLink :to="{name: 'recipe', params: {id: 3}}">Recipe 3</RouterLink>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <style scoped>
