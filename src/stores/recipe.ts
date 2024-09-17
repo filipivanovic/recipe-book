baedee64f489991825a7a6b4d7754722e5a855f2
Filip Ivanovic <filip_ivanovic@hotmail.com>
2024-09-17 17:40:09 +0200

    Add actions to pinia store

--- src/stores/recipe.ts
+++ src/stores/recipe.ts
@@ -6,11 +6,23 @@ interface Recipe {
   name: string,
   description: string
 }
+
+type NewRecipe = Omit<Recipe, 'id'>
+
 export const useRecipeStore = defineStore('recipe', () => {
   const recipes = ref<Recipe[]>([])
 
+  const addRecipe = (recipe: NewRecipe) => {
+    const newRecipe = {
+      id: Date.now().toString(),
+      ...recipe
+    };
+    recipes.value.push(newRecipe)
+    return newRecipe;
+  }
 
   return {
-    recipes
+    recipes,
+    addRecipe
   }
 })
--- src/views/AddRecipeView.vue
+++ src/views/AddRecipeView.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
 
 import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+import { useRecipeStore } from '@/stores/recipe'
 
+const store = useRecipeStore()
 const router = useRouter()
+const name = ref('')
+const description = ref('')
 
 const addRecipe = () => {
+  const recipe = store.addRecipe({
+    name: name.value,
+    description: description.value
+  });
   router.push({
     name: 'recipe',
     params: {
-      id: 1
+      id: recipe.id
     }
   })
 }
@@ -19,6 +28,12 @@ const addRecipe = () => {
 <template>
   <div>Add recipe</div>
   <form @submit.prevent="addRecipe">
+    <div>
+      <input type="text" v-model="name" placeholder="Recipe name" required>
+    </div>
+    <div>
+      <textarea v-model="description" placeholder="Recipe description" required></textarea>
+    </div>
     <button type="submit">Add</button>
   </form>
 </template>
